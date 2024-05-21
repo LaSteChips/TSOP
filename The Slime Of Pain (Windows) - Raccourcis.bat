4e988e592256194c69185949a84d751da8a2fbd5
LaSteChips <121933446+LaSteChips@users.noreply.github.com>
2024-05-21 17:05:01 +0200

    Update The Slime Of Pain (Windows) - Raccourcis.bat
    retour a la base raccourcis

--- The Slime Of Pain (Windows) - Raccourcis.bat
+++ The Slime Of Pain (Windows) - Raccourcis.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+set local
 
 rem Vérifier si Python est installé
 python --version >nul 2>&1
@@ -8,34 +8,18 @@ if %errorlevel% neq 0 (
     REM Télécharger et installer Python 3.11.9
     bitsadmin /transfer pythonDownloadJob /download /priority normal https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe %TEMP%\python-3.11.9-amd64.exe
     %TEMP%\python-3.11.9-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_launcher=1
-    
-    rem Mettre à jour le PATH pour la session actuelle
-    set "PATH=%PATH%;C:\Program Files\Python311\Scripts;C:\Program Files\Python311\"
-    
-    rem Vérifier si l'installation de Python a réussi
-    python --version >nul 2>&1
-    if %errorlevel% neq 0 (
-        echo L'installation de Python a échoué.
-        pause
-        exit /b 1
-    )
 ) else (
-    echo Python est bien installé
+    echo python est bien installer
 )
 
 rem Vérifier si Pygame est installé
 python -c "import pygame" >nul 2>&1
 if %errorlevel% neq 0 (
     echo Pygame n'est pas installé. Installation en cours...
-    rem Installer Pygame
+    REM Installer Pygame
     python -m pip install pygame
-    if %errorlevel% neq 0 (
-        echo L'installation de Pygame a échoué.
-        pause
-        exit /b 1
-    )
 ) else (
-    echo Pygame est bien installé
+    echo pygame est bien installer
 )
 
 rem Lancer votre jeu
